311a4abc753e5c13ea077cd5faf89840e029dc5c
AtomBalmBaby <bart@vandebeltmotorsports.com>
2024-08-11 11:36:09 -0400

    Add files via upload

---
+++ debtorpm.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+packagename=$1
+packagename=${packagename%.*}
+packagepath=$(realpath $packagename)
+
+echo $packagepath
+
+# extract the stuff
+alien -r --scripts --fixperms -g $packagename.deb
+
+#remove stupid directories that should not be in this file.
+
+sed -i 's#%dir "/"##' $packagename/*.spec
+sed -i 's#%dir "/usr/"##' $packagename/*.spec
+sed -i 's#%dir "/usr/bin/"##' $packagename/*.spec
+sed -i 's#%dir "/usr/share/"##' $packagename/*.spec
+sed -i 's#%dir "/usr/share/applications/"##' $packagename/*.spec
+
+# tell the package we don't care abour sch specific binaries in here
+
+sed -i '12 i %define _binaries_in_noarch_packages_terminate_build 0' $packagename/*.spec
+
+
+cd $packagename
+
+rpmbuild --target=noarch --buildroot $packagepath/ \
+         -bb $packagename-2.spec
+
+
